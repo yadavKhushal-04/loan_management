21076b9ad04f0a1f711aaab462faa26cad450a78
Khushal Yadav <khushaly04@gmail.com>
2025-08-02 01:22:26 +0530

    modifying

---
+++ Backend/src/DB/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+import {DB_NAME} from "../constants.js"
+
+const connectDB = async () => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+    }
+    catch(error){
+        console.log(`MongoDB connection error: ${error}`)
+        process.exit(1)
+    }
+}
---
+++ Backend/src/index.js
@@ -0,0 +1,19 @@
+import {app} from "/app.js"
+import connectDB from "./DB/index.js"
+import dotenv from "dotenv"
+
+
+//can also use "nodemon -r dotenv/config --experimental-json-modules src/index.js" in place of nodemon src/index.js in package.json
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+        .then(() => {
+            app.listen(process.env.PORT || 3000, () => {`App is listening at https://localhost:${process.env.PORT || 3000}`});
+
+        })
+        .catch((error) => {
+            console.log(`DB Connection error: ${error}`)
+        })
